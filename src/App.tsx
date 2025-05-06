9c27bf66648d1f681cc9f290bf18858b67ac1610
pirropirro <fra.pirrotta@gmail.com>
2025-05-06 19:23:11 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <span className="primary">The<span className="accent"> Failure</span> Company</span>
+      <br />
+      <span className="underline">COMING SOON</span>
+    </div>
+  )
+}
+
+export default App
